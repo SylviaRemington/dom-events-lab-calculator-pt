c5892db8f742ba062aac3819d3f4fb86bc96dac7
SylviaRemington <sylviaremington@gmail.com>
2025-04-18 01:14:53 -0700

    js set-up notes

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
 /*-------------------------------- Constants --------------------------------*/
 
+// Constants are variables that can’t be reassigned after they're set.
+// e.g. const name = 'Pikachu';
+
+const  = 
+
 /*-------------------------------- Variables --------------------------------*/
+
+// Variables are names you use to store information in your code.
+// e.g. let score = 100;
+
+let
 
 /*------------------------ Cached Element References ------------------------*/
 
+// Cached element references just means saving DOM elements in a variable 
+// so you don’t have to look them up again and again.
+// e.g. const button = document.querySelector('.my-button');
+
+
+
 /*----------------------------- Event Listeners -----------------------------*/
 
+// Event listeners are ways to make your webpage react to 
+// something—like a click, key press, or mouse move.
+// e.g. 
+
+// button.addEventListener('click', function() {
+//     console.log('Button was clicked!');
+//   });
+
+
 /*-------------------------------- Functions --------------------------------*/
+
+// Functions are blocks of code you can reuse. They do a specific job when you call them.
+// e.g. 
+// function greet() {
+//   console.log('Hello!');
+// }
+// greet(); 
+
